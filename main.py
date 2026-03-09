50cb86d6ec1b7fb60169c2dac53db6f518ea5597
iiinderr <gaggisingh5678@gmail.com>
2026-03-09 18:31:12 +0530

    testing the passwordhint

--- main.py
+++ main.py
@@ -1,6 +1,68 @@
-from models.database import Base, engine
+from models.database import Base, engine, SessionLocal
+from models.database import User, EncryptedNote, DocumentMetadata, UserRole , PasswordHint
 
 # create tables
 Base.metadata.create_all(bind=engine)
 
-print("Database created successfully")
+print("Tables created successfully")
+
+# create database session
+db = SessionLocal()
+
+
+# create a user
+user = User(
+    username="inder",
+    email="inder@test.com",
+    password_hash="hashedpassword123",
+    role=UserRole.ADMIN
+)
+
+db.add(user)
+db.commit()
+
+print("User inserted")
+
+
+# create encrypted note
+note = EncryptedNote(
+    title="My secret note",
+    encrypted_content="encrypted_text_here",
+    encryption_key_hint="family-key",
+    user_id=user.id
+)
+
+db.add(note)
+db.commit()
+
+print("Encrypted note inserted")
+
+
+# create document metadata
+doc = DocumentMetadata(
+    file_name="passport.pdf",
+    file_type="application/pdf",
+    file_size=300000,
+    encrypted_file_path="encrypted/path/file123",
+    tags="passport,personal",
+    user_id=user.id
+)
+
+db.add(doc)
+db.commit()
+
+print("Document metadata inserted")
+
+# create a passwordhint
+
+hint = PasswordHint(
+    service_name="Gmail",
+    encrypted_hint="encrypted_hint_example",
+    url="https://mail.google.com",
+    user_id=user.id
+)
+
+db.add(hint)
+db.commit()
+
+print("Password hint inserted")
